d85c3601966d46d5b275a9e6d9679abd2581d950
vvguerravv <victor.eg@aluno.ifsc.edu.br>
2024-11-22 08:54:23 -0300

    Starting my repository

---
+++ mic29004.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,14 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=mic29004.X.production.obj
+CND_ARTIFACT_PATH_default=dist/default/production/mic29004.X.production.obj
+# programming configuration
+CND_ARTIFACT_DIR_programming=dist/programming/production
+CND_ARTIFACT_NAME_programming=mic29004.X.production.obj
+CND_ARTIFACT_PATH_programming=dist/programming/production/mic29004.X.production.obj
